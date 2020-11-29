217073a9f85d49ae5226d7beea39135fd689ab07
Prasanna-Nanda <prasannanandakumar92@gmail.com>
2020-11-29 15:02:25 +0530

    Added UC1

--- regular.sh
+++ regular.sh
@@ -3,3 +3,9 @@
 
 read -p "Enter your name:" userFName
 pattern="(^[A-Z]{1})[a-z]{2,}$"
+if [[ $userFName =~ $pattern ]]
+then
+        echo "Yes pattern matched";
+else
+        echo "No pattern matched";
+fi
